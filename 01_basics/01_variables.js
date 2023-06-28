34024e76185ad59f6c617cd475a63b519c948678
Ashmit Das <137298461+Ashmit-1@users.noreply.github.com>
2023-06-28 11:58:04 +0000

    1st lesson of variables is completed

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 10
+let accountName = "Ashmit Das"
+let accountEmail = "ashmit.google.com"
+let accountCity = "Chandannagar"
+let accountPassword = 980890
+
+console.table([accountId, accountName, accountEmail, accountCity, accountPassword])
+
+accountCity = "Kolkata"
+accountEmail = "dasashmit.google.com"
+accountName = "Asmit Das"
+
+console.table([accountId, accountName, accountEmail, accountCity, accountPassword])
