ecdd1523ed9a8687f85a3adf82fbefecba52207e
Ashmit Das <137298461+Ashmit-1@users.noreply.github.com>
2023-06-29 14:13:16 +0000

    datatype conversions

---
+++ 01_basics/03_datatype_conversion.js
@@ -0,0 +1,7 @@
+let int = null
+let someNumber = Number(int)
+let someString = String(int)
+let someBoolean = Boolean(int)
+console.table([int, someNumber, someString, someBoolean])
+
+// null gives null as string.
