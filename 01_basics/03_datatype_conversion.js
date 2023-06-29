c7ccf07665c1c5695344bc79e755f62944350469
Ashmit Das <137298461+Ashmit-1@users.noreply.github.com>
2023-06-29 14:59:48 +0000

    datatype concepts cleared

--- 01_basics/03_datatype_conversion.js
+++ 01_basics/03_datatype_conversion.js
@@ -2,6 +2,34 @@ let int = null
 let someNumber = Number(int)
 let someString = String(int)
 let someBoolean = Boolean(int)
-console.table([int, someNumber, someString, someBoolean])
+//console.table([int, someNumber, someString, someBoolean])
 
 // null gives null as string.
+
+//*************************************Operations****************************************
+
+console.log(5 % 3);  // % gives remainder as return
+console.log(2 ** 3); // ** gives power 
+console.log(true + true);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+let x2 = 3;
+const y2 = x2++;
+
+console.log(`x2: ${x2}, y2: ${y2}`);
+
+console.log(`The value of x2 is ${x2}`); // This is the way to get both a string and a previously number/string in input.
+
+const fish = ["Lion", , "Angel"];
+console.log(fish) // This gives [ 'Lion', <1 empty item>, 'Angel' ] as output
