1325cc28de58441418271c546be0fe318d087eaa
Ashmit Das <137298461+Ashmit-1@users.noreply.github.com>
2023-07-15 06:05:17 +0000

    Array Basics

---
+++ 02_basics/01_array.js
@@ -0,0 +1,25 @@
+let myArrOne = [0, 1, 6, 9]
+let myArrTwo = [2, 3, 4, 5]
+let myArrThree = [7, 8]
+let myArrFour = [myArrOne, myArrTwo, myArrThree, 10, 11]
+//console.log(myArrFour[1]);
+let arrElementOne = Math.floor((Math.random()*51)+1)
+let arrElementTwo = Math.floor((Math.random()*51)+1)
+let arrElementThree = Math.floor((Math.random()*51)+1)
+let arrElementFour = Math.floor((Math.random()*51)+1)
+let arrElementFive = Math.floor((Math.random()*51)+1)
+let arrElementSix = Math.floor((Math.random()*51)+1)
+let myArr = new Array(arrElementOne, arrElementTwo, arrElementThree, arrElementFour, arrElementFive, arrElementSix )
+console.log(myArr);
+// myArr.push(Math.floor((Math.random()*51)+1))
+// console.log(myArr);
+// myArr.pop()
+// console.log(myArr);
+// myArr.unshift(Math.floor((Math.random()*51)+1))
+// console.log(myArr);
+// myArr.shift()
+// myArr.shift()
+// console.log(myArr);
+// console.log(myArr.length);
+
+
