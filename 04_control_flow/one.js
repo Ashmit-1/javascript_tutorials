028a6a6374c51bb16f912628a09250bd390bd006
Ashmit Das <137298461+Ashmit-1@users.noreply.github.com>
2023-07-27 12:08:56 +0000

    basic complete

---
+++ 04_control_flow/one.js
@@ -0,0 +1,15 @@
+if (2 !== "2") {         
+    console.log("The code is executed!!");
+}
+else{
+    console.log("The code is not executed !!");
+}
+
+let userScore = 50
+if (userScore > 100) {
+    let userPower = "swim"
+    console.log(`The user power is ${userPower}`);
+} else {
+    console.log(`User doesn't any power `);
+    
+}
