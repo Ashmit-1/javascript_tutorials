588aced03e19abdd8f5645f7b7ab85d7e8e43eab
Ashmit Das <137298461+Ashmit-1@users.noreply.github.com>
2023-07-30 12:18:46 +0000

    loops

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -13,3 +13,50 @@ if (userScore > 100) {
     console.log(`User doesn't any power `);
     
 }
+let operator = "*"
+
+switch (operator) {
+    case "+" :
+        addition(5, 6)
+        break;
+    
+    case "-":
+        subtraction(10, 5)
+        break;
+    
+    case "*":
+        multiplication(10, 5)
+        break;
+    case "/":
+        division(10, 5)
+        break;
+
+    default:
+        console.log("Enter a valid operand");
+        break;
+}
+
+function addition(a, b) {
+    console.log(a + b); 
+}
+
+function subtraction(a, b) {
+    console.log(a - b);
+    
+}
+
+function multiplication(a, b) {
+    console.log(a * b);
+    
+}
+
+function division(a, b) {
+    console.log(a / b);
+    
+}
+
+//Ternary Operator
+
+let marks = 7
+
+marks >= 40 ? console.log("Student passed the exam") : console.log("Student failed the exam");
---
+++ 05_loops/one.js
@@ -0,0 +1,15 @@
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${i}`);
+    
+// }
+
+//TABLE OF NUMBERS FROM 1 TO 12 
+
+for (let i = 1; i <= 12; i++) {
+    console.log(`This is the table of ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        console.log(`${i} * ${j} = ${i*j}`);
+        
+    }
+    
+}
